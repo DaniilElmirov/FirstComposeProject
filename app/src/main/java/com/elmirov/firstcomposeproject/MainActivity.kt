7eced7586035b1f8b448db3c76c02c27c634ef7d
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-11-29 23:05:31 +0700

    Работа с Text()

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -5,9 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.elmirov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.elmirov.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -26,5 +37,45 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun TextTest() {
+    Column {
+        Text(
+            text = "Hello World",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Serif,
+            textDecoration = TextDecoration.combine(
+                listOf(
+                    TextDecoration.Underline,
+                    TextDecoration.LineThrough
+                )
+            )
+        )
+
+        Text(
+            buildAnnotatedString {
+                withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                    append("Hello")
+                }
+
+                withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+                    append(" ")
+                }
+
+                withStyle(
+                    SpanStyle(
+                        textDecoration = TextDecoration.LineThrough,
+                        fontSize = 20.sp
+                    )
+                ) {
+                    append("World!")
+                }
+            }
+        )
     }
 }
