69edd58ce4df8fafbfd6a1fc42188caa905abd81
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-11-29 16:18:22 +0700

    Column

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -3,9 +3,9 @@ package com.elmirov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -21,19 +21,15 @@ class MainActivity : ComponentActivity() {
 
 @Preview //Работает только с composable-функциями без параметров
 @Composable
-fun Hello() {
-    Text(text = "Hello!", color = Color.Green)
-}
-
-@Preview
-@Composable
 fun UserInfoPreview() {
     UserInfo("NamePreview", 222)
 }
 
 @Composable
 fun UserInfo(name: String, age: Int) {
-    Text(
-        text = "Hello, $name. Your age: $age",
-    )
+    Column {
+        repeat(10) {
+            Text(text = "Hello, $name. Your age: $age")
+        }
+    }
 }
