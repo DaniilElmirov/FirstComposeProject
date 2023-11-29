ba43b35852f489773a167fea531a3da5db48b32a
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-11-29 15:35:37 +0700

    Знакомство с compose

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -3,44 +3,23 @@ package com.elmirov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.elmirov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            FirstComposeProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            UserInfo(name = "Name", age = 22)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun UserInfo(name: String, age: Int) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello, $name. Your age: $age",
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstComposeProjectTheme {
-        Greeting("Android")
-    }
 }
