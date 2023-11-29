52950d2fa26e723c25d8d5d197b906f4a3316f3f
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-11-29 17:51:53 +0700

    Card

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -3,6 +3,17 @@ package com.elmirov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.elmirov.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
@@ -12,5 +23,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             InstagramProfileCard()
         }
+    }
+}
+
+@Preview
+@Composable
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        colors = CardDefaults.cardColors(Color.Blue),
+        border = BorderStroke(width = 1.dp, color = Color.Black)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(all = 8.dp),
+            text = "Text text Text",
+        )
     }
 }
