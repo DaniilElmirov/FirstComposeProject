108358eaf00d717d586ae3a0ea9ffb5786b0b52a
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-11-29 22:44:51 +0700

    Использовал цвета в InstagramProfileCard

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -3,6 +3,12 @@ package com.elmirov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Modifier
+import com.elmirov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.elmirov.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +16,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            InstagramProfileCard()
+            FirstComposeProjectTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    InstagramProfileCard()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/elmirov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,15 +21,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun InstagramProfileCard() {
     Card(
         modifier = Modifier
             .padding(8.dp),
         shape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp),
-        colors = CardDefaults.cardColors(Color.White),
-        border = BorderStroke(width = 1.dp, color = Color.Black),
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background),
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.onBackground),
     ) {
         Row(
             modifier = Modifier
@@ -69,5 +69,25 @@ private fun TwoVerticalBoxes() {
                 .size(25.dp)
                 .background(Color.Green)
         )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewCardLight() {
+    FirstComposeProjectTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewCardDark() {
+    FirstComposeProjectTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
     }
 }
