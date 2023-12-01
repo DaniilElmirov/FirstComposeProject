c3d11ea39fe470911c77afcc8fd221aa41a01cee
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-01 14:53:08 +0700

    Сделал заголовок для PostCard

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -3,44 +3,30 @@ package com.elmirov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.elmirov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
+import com.elmirov.firstcomposeproject.ui.theme.PostCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FirstComposeProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
+                        .fillMaxSize(),
                 ) {
-                    Greeting("Android")
+                    PostCard()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstComposeProjectTheme {
-        Greeting("Android")
     }
 }
