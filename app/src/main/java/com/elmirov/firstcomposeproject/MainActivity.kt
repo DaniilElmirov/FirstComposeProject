456d2463e40c3d558a586b5610587393f26d2c39
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-01 17:34:24 +0700

    PostCard готова

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.elmirov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.elmirov.firstcomposeproject.ui.theme.PostCard
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
             FirstComposeProjectTheme {
                 Box(
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
+                        .background(Color.Gray)
                         .padding(8.dp)
                         .fillMaxSize(),
                 ) {
