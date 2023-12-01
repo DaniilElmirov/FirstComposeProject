00f238a4a2567bb1a7d56bbb3ef97f7a04f6f06f
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-01 20:36:54 +0700

    Поправил зависимость. Material Components

--- app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/elmirov/firstcomposeproject/MainActivity.kt
@@ -3,30 +3,81 @@ package com.elmirov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.elmirov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
-import com.elmirov.firstcomposeproject.ui.theme.PostCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeProjectTheme {
-                Box(
-                    modifier = Modifier
-                        .background(Color.Gray)
-                        .padding(8.dp)
-                        .fillMaxSize(),
-                ) {
-                    PostCard()
-                }
-            }
+            Test()
         }
     }
+}
+
+@Composable
+private fun Test() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column {
+            Example1()
+            Example2()
+            //Example3()
+        }
+    }
+}
+
+@Composable
+private fun Example1() {
+    OutlinedButton(onClick = {}) {
+        Text(text = "Hello World")
+    }
+}
+
+@Composable
+private fun Example2() {
+    TextField(
+        value = "Value",
+        onValueChange = {},
+        label = { Text(text = "Label") }
+    )
+}
+
+@Composable
+private fun Example3() {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Yes"
+            )
+        },
+        title = {
+            Text(
+                text = "Are you sure?"
+            )
+        },
+        text = {
+            Text(text = "Do you want to delete this file?")
+        },
+        dismissButton = {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "No"
+            )
+        }
+    )
 }
