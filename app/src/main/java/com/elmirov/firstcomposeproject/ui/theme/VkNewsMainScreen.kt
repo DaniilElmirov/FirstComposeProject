439d8299242b6125d7a4bf5b784baa0b26314cca
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2023-12-02 14:16:37 +0700

    Стейты и рекомпозиция на примере NavigationBar

---
+++ app/src/main/java/com/elmirov/firstcomposeproject/ui/theme/VkNewsMainScreen.kt
@@ -0,0 +1,59 @@
+package com.elmirov.firstcomposeproject.ui.theme
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.elmirov.firstcomposeproject.ui.theme.NavigationItem.Favorite
+import com.elmirov.firstcomposeproject.ui.theme.NavigationItem.Home
+import com.elmirov.firstcomposeproject.ui.theme.NavigationItem.Profile
+
+@Preview
+@Composable
+fun MainScreen() {
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val items = listOf(Home, Favorite, Profile)
+                val selectedItemPosition = rememberSaveable {
+                    mutableStateOf(0)
+                }
+
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = index == selectedItemPosition.value,
+                        onClick = { selectedItemPosition.value = index },
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = stringResource(id = item.titleResId))
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                            selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            unselectedIconColor = MaterialTheme.colorScheme.onSecondary,
+                            unselectedTextColor = MaterialTheme.colorScheme.onSecondary,
+                            indicatorColor = MaterialTheme.colorScheme.primary
+                        ),
+                    )
+                }
+            }
+        }
+    ) {
+        Text(
+            modifier = Modifier.padding(it),
+            text = ""
+        )
+    }
+}
